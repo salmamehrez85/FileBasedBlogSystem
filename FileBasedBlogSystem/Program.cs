e2d40e568b2c464b4f50cc885dee72231fc55255
salmamehrez85 <salmamehrez85@gmail.com>
2025-06-03 00:30:03 +0300

    Added Category and Tag Filtering & Search Endpoint

--- FileBasedBlogSystem/Program.cs
+++ FileBasedBlogSystem/Program.cs
@@ -124,20 +124,60 @@ app.MapGet("/editor", () => Results.Ok("Author or Editor area"))
    .RequireAuthorization("AuthorOrEditor");
 
 
-
-app.MapGet("/posts", ([FromServices]PostService postService) =>
+app.MapGet("/posts", (int? page, int? pageSize, [FromServices]PostService postService) =>
 {
+    var currentPage = page ?? 1;
+    var currentPageSize = pageSize ?? 10;
+    
     var posts = postService.GetAllPosts()
-        .Where(p => p.Status == "published");
+        .Where(p => p.Status == "published")
+        .OrderByDescending(p => p.PublishedDate)
+        .Skip((currentPage - 1) * currentPageSize)
+        .Take(currentPageSize);
     return Results.Ok(posts);
 });
 
 
-app.MapGet("/posts/{slug}", (string slug, [FromServices]PostService postService) =>
+app.MapGet("/posts/{slug}", (string slug, [FromServices] PostService postService) =>
 {
     var post = postService.GetPostBySlug(slug);
     return post is not null ? Results.Ok(post) : Results.NotFound();
 });
+
+
+
+app.MapGet("/posts/category/{category}", (string category, [FromServices]PostService postService) =>
+{
+    var posts = postService.GetAllPosts()
+        .Where(p => p.Status == "published" && p.Categories.Contains(category, StringComparer.OrdinalIgnoreCase))
+        .OrderByDescending(p => p.PublishedDate);
+
+    return Results.Ok(posts);
+});
+
+
+app.MapGet("/posts/tag/{tag}", (string tag, [FromServices]PostService postService) =>
+{
+    var posts = postService.GetAllPosts()
+        .Where(p => p.Status == "published" && p.Tags.Contains(tag, StringComparer.OrdinalIgnoreCase))
+        .OrderByDescending(p => p.PublishedDate);
+
+    return Results.Ok(posts);
+});
+
+
+app.MapGet("/posts/search", (string q, [FromServices]PostService postService) =>
+{
+    var posts = postService.GetAllPosts()
+        .Where(p => p.Status == "published" &&
+            (p.Title.Contains(q, StringComparison.OrdinalIgnoreCase) ||
+             p.Description.Contains(q, StringComparison.OrdinalIgnoreCase) ||
+             p.Body.Contains(q, StringComparison.OrdinalIgnoreCase)))
+        .OrderByDescending(p => p.PublishedDate);
+
+    return Results.Ok(posts);
+});
+
 
 
 app.MapPost("/posts", [Authorize(Roles = "Author,Admin")] (BlogPost post, HttpContext context, [FromServices]PostService postService) =>

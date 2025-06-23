3ac1d39e8e3de2c6a7dc5da7f26ede8d747f9f2d
salmamehrez85 <salmamehrez85@gmail.com>
2025-06-24 00:17:44 +0300

    Add Max Length Validation To BlogPost

--- FileBasedBlogSystem/Features/BlogPost.cs
+++ FileBasedBlogSystem/Features/BlogPost.cs
@@ -1,18 +1,23 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 namespace FileBlogSystem.Features;
 
 public class BlogPost
 {
+    [MaxLength(100)]
     public string Title { get; set; } = string.Empty;
+    [MaxLength(500)]
     public string Description { get; set; } = string.Empty;
     public string Body { get; set; } = string.Empty;
     public DateTime PublishedDate { get; set; }
     public DateTime ModifiedDate { get; set; }
     public List<string> Tags { get; set; } = [];
     public List<string> Categories { get; set; } = [];
+    [MaxLength(100)]
     public string Slug { get; set; } = string.Empty;
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public PostStatus Status { get; set; } = PostStatus.Draft;
+    [MaxLength(50)]
     public string Author { get; set; } = string.Empty;
 }
 
